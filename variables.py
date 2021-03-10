208a6018ab24ca47f0c357325372d5497d1180d7
bnagle22 <80408236+bnagle22@users.noreply.github.com>
2021-03-10 10:03:47 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 16 14:18:04 2021
+
+@author: benjamin.nagle
+"""
+
+a= 2 + 5
+a = a + a + a
+b = a * 2
+c = b ** b
+d = a + b / b
+e = (a + b) / b
+#f = w + b + a
+g = a * a * a* a / a /a /a /a
+w = g + 1 + 3 * a
+#z = five + five
+x ="car" * 3
+
+print(a)
+print(b)
+print(c)
+
+print(d)
+print(e)
+#print(f)
+print(g)
+
+print(w)
+print(x)
+#print(z)
